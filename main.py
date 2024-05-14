106a9439124012076d0e6b6f7f72347d5fcbda30
mohammadrezaDahaghin <kitkat9069@gmail.com>
2024-05-14 08:23:58 +0330

    Fixed bugs observed in test mode

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from telebot import types
 import os
 import schedule
 import time
+import getpass
 
 
 #telebot Token
@@ -21,14 +22,19 @@ base_url = 'https://jira.basa.ir'
 
 # Authentication endpoint
 auth_url = f'{base_url}/jira/rest/auth/1/session'
+# searchIsue endpoint
 getissue_url = f'{base_url}/jira/rest/api/2/search'
 
-os.environ['SESSION_VALUE'] = ''
-os.environ['JIRA_USERNAME'] = ''
-os.environ['JIRA_PASSWORD'] = ''
-os.environ['CHAT_ID'] = ''
-previous_issue_count = 0
-previous_issues = {}
+os.environ['SESSION_VALUE'] = ''     # Jira session token
+os.environ['JIRA_USERNAME'] = ''     # Jira username
+os.environ['JIRA_PASSWORD'] = ''     # Jira password
+os.environ['CHAT_ID'] = ''           # Telegram chat ID
+
+# Initialize variables for tracking issues and user actions
+previous_issue_count = 0             # Count of previous issues
+previous_issues = {}                 # Dictionary to store previous issues
+last_start_command_time = {}          # Dictionary to track the time of last /start command
+user_logged_in = {}                  # Dictionary to track user login status
 
 
 headers = {
@@ -36,7 +42,9 @@ headers = {
   "Content-Type": "application/json"
 }
 
+    #handle Request auth
 def authenticate_and_get_session(username, password):
+
     data = {"username": username, "password": password}
     response = requests.post(auth_url, json=data, headers=headers)
     if response.status_code == 200:
@@ -50,7 +58,7 @@ def authenticate_and_get_session(username, password):
 
 def get_issues(session_value):
     IssueLoad = json.dumps({
-        "jql": f"assignee = {username}",
+        "jql": f"assignee = {username} AND statusCategory = 2",
         "maxResults": None
     })
 
@@ -69,21 +77,40 @@ def get_issues(session_value):
     else:
         return None
 
+
+    #handle start comment
 @bot.message_handler(commands=['start'])
 def start_message(message):
+
+    global last_start_command_time
+
+    user_id = message.chat.id
+    current_time = time.time()
+    time_limit = 15  # Time limit in seconds
+
+    if os.getenv('SESSION_VALUE') in last_start_command_time:
+        last_time = last_start_command_time[os.getenv('SESSION_VALUE')]
+        if current_time - last_time < time_limit:
+            bot.send_message(message.chat.id, "شما نمی‌توانید `/start` را بیش از یک بار در {} ثانیه ارسال کنید.".format(time_limit))
+            return
+
+    last_start_command_time[user_id] = current_time
+
     os.environ['CHAT_ID'] =str(message.chat.id)
-    sent_msg = bot.send_message(message.chat.id, "نام کاربری خود را وارد کنید:")
+    print(f"User not logged in yet!{os.getenv('CHAT_ID')}")
+    sent_msg = bot.send_message(message.chat.id, "\U00002b55 نام کاربری خود را وارد کنید:")
     bot.register_next_step_handler(sent_msg, username_handler)
+
 
 def username_handler(message):
 
     global username
 
     username = message.text
-    sent_msg = bot.send_message(message.chat.id, "کلمه عبور را وارد کنید:")
+    sent_msg = bot.send_message(message.chat.id, "\U00002b55 کلمه عبور خود را وارد کنید:")
     bot.register_next_step_handler(sent_msg, secure_handler)
 
-
+    # Task to be scheduled.
 def Task():
     global previous_issue_count
     global previous_issues
@@ -94,13 +121,13 @@ def Task():
             issue_count = len(issues)
             if issue_count > previous_issue_count:
                 new_issues_count = issue_count - previous_issue_count
-                message_text = f"تعداد مسائل جدید در این دقیقه: {new_issues_count}\n"
+                message_text = f"تعداد تسک های باز شما در حال حاضر\U00002714 مورد\n: {new_issues_count}\n"
                 for issue in issues:
                     if issue['key'] not in previous_issues:
                         issue_key = issue['key']
                         summary = issue['fields']['summary']
                         creator = issue['fields']['creator']['displayName']
-                        message_text += f"Issue Key: {issue_key}\n Summary: {summary}\n Creator: {creator}\n\n"
+                        message_text += f"\U000027a1 Issue Key: {issue_key}\n \U0001f300 Summary: {summary}\n \U0001f300 Creator: {creator}\n\n"
                         previous_issues[issue_key] = issue
                 bot.send_message(os.getenv('CHAT_ID'), message_text)
                 previous_issue_count = issue_count
@@ -108,35 +135,50 @@ def Task():
                 previous_issue_count = issue_count
 
 
+# Handle the user's response to the password prompt.
+
 def secure_handler(message):
 
     global secure
 
-    secure = message.text
+    secure =message.text
     hashed_secure = hashlib.sha256(secure.encode()).hexdigest()
     session_token = authenticate_and_get_session(username, secure)
 
     if session_token:
-        bot.send_message(message.chat.id,f"احراز هویت با موفقیت انجام شد{os.getenv('JIRA_USERNAME')}.")
+        bot.send_message(message.chat.id,f"{os.getenv('JIRA_USERNAME')} احراز هویت شما با موفقیت انجام شد.")
+
+
         issues = get_issues(session_token)
+        if len(issues)==0:
+            bot.send_message(message.chat.id, " \U0000203C برای شما Issue در وضعیت TO DO وجود ندارد ")
+        else:
+            bot.send_message(message.chat.id, " \U00002747 آخرین Issue های با وضعیت کلی TO DO برای شما")
+            if issues:
+                for issue in issues:
+                    issue_key = issue['key']
+                    summary = issue['fields']['summary']
+                    creator = issue['fields']['creator']['displayName']
+                    message_text = f"\U000027a1 Issue Key: {issue_key}\n \U0001f300 Summary: {summary}\n \U0001f300 Creator: {creator}"
+
+                    bot.send_message(message.chat.id, message_text)
+                    print(os.getenv('CHAT_ID'))
 
 
-        if issues:
-            for issue in issues:
-                issue_key = issue['key']
-                summary = issue['fields']['summary']
-                creator = issue['fields']['creator']['displayName']
-                message_text = f" Issue Key: {issue_key}\n Summary: {summary}\n Creator: {creator}"
-                bot.send_message(message.chat.id, message_text)
-                print(os.getenv('CHAT_ID'))
 
         schedule.every(1).minutes.do(Task)
 
         while True:
             schedule.run_pending()
             time.sleep(1)
+    else:
+        bot.send_message(message.chat.id, "\U0000203C کلمه عبور و یا نام کاربری خود را بدرستی وارد کرده\n و یا اتصال اینترنت خود را چک کنید\n در صورت اطمینان از عدم مشکل از سمت شما با پشتیبانی تماس بگیرید")
 
 
+# handle other comment
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, "\U00002734 لطفاً ابتدا دستور /start را ارسال کنید.")
 
 
 bot.polling()
